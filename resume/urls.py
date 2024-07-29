97c5674b949d0332310fbb347fb91f0b3326a14e
vivekkantariya <kantariyavivek75@gmail.com>
2024-07-29 16:32:40 +0530

    add

--- resume/urls.py
+++ resume/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path 
 from . import views 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -10,3 +12,6 @@ urlpatterns = [
     path('create-yours/', views.create_yours, name='create_yours'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
